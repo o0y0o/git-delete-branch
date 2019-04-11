d800f1556cb21a2eaebb2cc6b36e2cdfdb78b661
jason.chung <shiningjason1989@gmail.com>
2019-04-12 00:07:39 +0800

    1.0.7

--- cli.js
+++ cli.js
@@ -34,7 +34,7 @@ function startSpinner(text) {
   const repo = git().silent(true)
 
   const stopSpinner = startSpinner('fetching...')
-  await repo.fetch({ '--all': null, '-p': null })
+  await repo.fetch(['--all', '-p'])
   const { branches } = await repo.branch()
   stopSpinner()
 
@@ -69,7 +69,7 @@ function startSpinner(text) {
         await repo.branch(['-D', branch])
       } else {
         const [, remoteName, localName] = matches
-        await repo.push(remoteName, localName, { '--delete': null })
+        await repo.push(remoteName, localName, ['--delete'])
       }
       stopSpinner()
       console.log(`${branch} ${chalk.green.bold('Success')}`)
